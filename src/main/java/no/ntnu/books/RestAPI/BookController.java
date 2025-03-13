8055a01c7c6e596c93b30d7f1746c32bbd59051e
pahmel0 <mantasx5@gmail.com>
2025-03-13 22:00:42 +0100

    Update Java version to 17, add MySQL and JPA dependencies, and implement logging in BookController

--- src/main/java/no/ntnu/books/RestAPI/BookController.java
+++ src/main/java/no/ntnu/books/RestAPI/BookController.java
@@ -1,4 +1,6 @@
 package no.ntnu.books.RestAPI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
  */
 public class BookController {
 
+    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+
     private List<Book> books;
 
     public BookController() {
@@ -37,6 +41,7 @@ public class BookController {
 
     @GetMapping
     public List<Book> getAllBooks() {
+        logger.info("Retrieving all books");
         return books;
     }
 
